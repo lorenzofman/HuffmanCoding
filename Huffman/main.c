f1996e1f17dc97a8cf65ff90a8ad1b01c022b589
lorenzofmann <lorenzofman@gmail.com>
2019-06-25 21:30:08 -0300

    Filter only valid occurrences

--- Huffman/main.c
+++ Huffman/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef FILE File;
 int *FindOccurrences(File* file)
 {
@@ -13,13 +12,33 @@ int *FindOccurrences(File* file)
 	}
 	return occurrences;
 }
+
+int* FilterOccurrences(int* occurrences)
+{
+	int count = 0;
+	for (int i = 0; i < 256; i++)
+	{
+		if (occurrences[i])
+		{
+			count++;
+		}
+	}
+	int* filtered = malloc(sizeof(int) * 256);
+	int j = 0;
+	for (int i = 0; i < 256; i++)
+	{
+		if (occurrences[i])
+		{
+			filtered[j++] = occurrences[i];
+		}
+	}
+	return filtered;
+}
 int main()
 {
 	File* file;
 	file = fopen("test.txt", "r");
 	int* occurrences = FindOccurrences(file);
-	for (int i = 0; i < 256; i++)
-	{
-		printf("%c (%i)\n",(char) i, occurrences[i]);
-	}
+	int* filteredOccurrences = FilterOccurrences(occurrences);
+	free(occurrences);
 }
